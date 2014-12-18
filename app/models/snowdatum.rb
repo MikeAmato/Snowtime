31a00e17801eace875e9e4a7410f500c48924997
MikeAmato <mike.blackmist@aemail4u.com>
2014-12-18 16:31:29 -0500

    precompiled assets locally

--- app/models/snowdatum.rb
+++ app/models/snowdatum.rb
@@ -9,8 +9,8 @@ class Snowdatum < ActiveRecord::Base
 
 
     #to call the second api to get the snow depth data
-  @response2 = HTTParty.get('https://api.weathersource.com/v1/0d0035b08d9f3987a068/
-    history_by_postal_code.json?period=day&postal_code_eq=80424&country_eq=US&
-    timestamp_eq=2010-12-19T00:00:00&
-    fields=postal_code,country,timestamp,precip,snowfall')
+  # @response2 = HTTParty.get('https://api.weathersource.com/v1/0d0035b08d9f3987a068/
+  #   history_by_postal_code.json?period=day&postal_code_eq=80424&country_eq=US&
+  #   timestamp_eq=2010-12-19T00:00:00&
+  #   fields=postal_code,country,timestamp,precip,snowfall')
 end
